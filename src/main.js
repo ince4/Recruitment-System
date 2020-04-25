a9a07fc602a711b0e00c1499ea5aca53317c9288
ince4 <421603832@qq.com>
2020-04-25 16:32:17 +0800

    initialized

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import {
+  Button,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+
 Vue.config.productionTip = false
 
 new Vue({
